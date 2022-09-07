69bc725ba04c127672366a916f67c7268fabb675
tracer-nju <tracer@nju.edu.cn>
2022-09-07 11:42:01 +0800

    Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:42:01 up 5 days,  2:12,  4 users,  load average: 0.08, 0.07, 0.02

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,6 +2,11 @@
 #include <assert.h>
 
 int main(int argc, char *argv[]) {
+  // arg part
+  int _numeric_out = 0;
+  int _pid_out = 0;
+  int _version_out = 0;
+
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
