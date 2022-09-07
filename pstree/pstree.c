36bebb8524f7d71c22ba27fbb71055bd47944634
tracer-nju <tracer@nju.edu.cn>
2022-09-07 11:55:44 +0800

    Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:55:44 up 5 days,  2:26,  4 users,  load average: 0.07, 0.05, 0.01

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -9,12 +9,28 @@ int main(int argc, char *argv[]) {
 
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+    if(*argv[i] == "-n"||"--numeric-sort")
+      _numeric_out = 1;
+    else if(*argv[i] == "-p"||"--show-pids")
+      _pid_out = 1;
+    else if(*argv[i] == "-V"||"--version")
+      _version_out = 1;
+    else
+    {
+      printf("Wrong arg input!\n");
+      return 0;
+    }
   }
-
   assert(!argv[argc]);
 
-  char *words = "It is a string";
-  printf("%s",words);
+  char *version_info = 
+    "This is my first version of pstree!\n ";
+  if(_version_out)
+  {
+    printf("%s", version_info);
+    return 0;
+  }
+
+
   return 0;
 }
