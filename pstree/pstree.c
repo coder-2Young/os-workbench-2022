5fc5e51f8eff476ac311fc599fc69ede63a61fd9
tracer-nju <tracer@nju.edu.cn>
2022-09-08 10:36:22 +0800

    Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:36:22 up 6 days, 35 min,  3 users,  load average: 0.03, 0.05, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -68,7 +68,7 @@ char* cut_proc_name(char *proc_name)
 Process* add_children_tree(char* pid)
 {
   char root_path[MAX_PATH_LEN];
-  strcat(strcat(root_path,"/proc/"),pid);
+  strcat(strcat(root_path,"./proc/"),pid);
   
   char status_path[MAX_PATH_LEN];
   strcat(strcat(status_path,root_path),"/status");
