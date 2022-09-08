52f5804a413fd72687f622a0a9617ff9286b7a91
tracer-nju <tracer@nju.edu.cn>
2022-09-08 10:38:46 +0800

    Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:38:46 up 6 days, 38 min,  3 users,  load average: 0.00, 0.03, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -73,23 +73,23 @@ Process* add_children_tree(char* pid)
   char status_path[MAX_PATH_LEN]="";
   strcat(strcat(status_path,root_path),"/status");
   char process_name[MAX_PROC_NAME_LEN];
-  // FILE *status = fopen(status_path,"r");
-  // if(status == NULL)
-  // {
-  //   perror("Error opening file");
-  //   return(NULL);
-  // }
-  // fgets(process_name,MAX_PROC_NAME_LEN, status);
-  // fclose(status);
-  // char* name = cut_proc_name(process_name);
-  // Process *root = init_process(pid,name);
+  FILE *status = fopen(status_path,"r");
+  if(status == NULL)
+  {
+    perror("Error opening file");
+    return(NULL);
+  }
+  fgets(process_name,MAX_PROC_NAME_LEN, status);
+  fclose(status);
+  char* name = cut_proc_name(process_name);
+  Process *root = init_process(pid,name);
 
-  // char* children_path = root_path;
-  // strcat(strcat(strcat(children_path,"/task/"),pid),"/children");
+  char* children_path[MAX_PATH_LEN];
+  strcat(strcat(strcat(strcat(children_path,root_path),"/task/"),pid),"/children");
 
   printf("root path: %s\n",root_path);
   printf("status path: %s\n",status_path);
-  // printf("children path: %s",children_path);
+  printf("children path: %s",children_path);
 
 
   // travel through children
